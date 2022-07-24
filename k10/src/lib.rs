937c3cb01ece3ec08e8926e0d1bb1ebbe4cd1587
erde74 <git@alarum.de>
2022-07-24 16:33:40 +0200

    cleanup

--- k10/src/lib.rs
+++ k10/src/lib.rs
@@ -2,6 +2,7 @@
 #![feature(custom_test_frameworks)]
 #![feature(unchecked_math)]
 #![feature(const_mut_refs)]
+#![feature(asm_const)]
 #![allow(bad_asm_style, unused_unsafe)]
 #![allow(dead_code, unused_macros)]
 #![allow(non_upper_case_globals)]
